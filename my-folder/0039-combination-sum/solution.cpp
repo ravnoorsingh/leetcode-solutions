2ecf5f53f532220e41ed06d27fd77416d6e99663
ravnoorsingh <160342867+ravnoorsingh@users.noreply.github.com>
2024-06-04 20:57:18 +0000

    [LeetCode Sync] Runtime - 25 ms (22.88%), Memory - 28 MB (19.31%)

---
+++ my-folder/0039-combination-sum/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    void check(vector<vector <int> > &vec,vector<int> ans,vector<int> candidates,int idx,int target){
+        if(target<0) return;
+        if(target==0){
+            vec.push_back(ans);
+            return;
+        }
+        for(int i=idx;i<candidates.size();i++){
+            ans.push_back(candidates[i]);
+            check(vec,ans,candidates,i,target-candidates[i]);
+            ans.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector <int> > vec;
+        vector<int> ans;
+        int idx = 0 ;
+        check(vec,ans,candidates,idx,target);
+        return vec;
+    }
+};
