51b1e792528fb3b84bb5f59b20817544530e13dd
ravnoorsingh <160342867+ravnoorsingh@users.noreply.github.com>
2024-07-05 13:25:20 +0000

    [LeetCode Sync] Runtime - 15 ms (7.18%), Memory - 27.8 MB (49.98%)

--- my-folder/0169-majority-element/solution.cpp
+++ my-folder/0169-majority-element/solution.cpp
@@ -1,29 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int candidate = 0;
-
-        // Find the candidate for majority element
-        for(int num : nums) {
-            if(count == 0) {
-                candidate = num;
-            }
-            count += (num == candidate) ? 1 : -1;
-        }
-
-        // Verify the candidate
-        count = 0;
-        for(int num : nums) {
-            if(num == candidate) {
-                count++;
-            }
-        }
-
-        if(count > nums.size() / 2) {
-            return candidate;
-        }
-
-        return -1;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return nums[n/2];
     }
 };
