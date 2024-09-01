30708f7aa733369392dc3a1d2b6172f5fb9e269b
ravnoorsingh <160342867+ravnoorsingh@users.noreply.github.com>
2024-09-01 06:55:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (86.79%)

---
+++ my-folder/0020-valid-parentheses/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2!=0) return false;
+        stack <char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                if(st.size()==0) return false;
+                if(st.top()=='(') st.pop();
+                else return false;
+            }
+            else if(s[i]==']'){
+                if(st.size()==0) return false;
+                if(st.top()=='[') st.pop();
+                else return false;
+            }
+            else if(s[i]=='}'){
+                if(st.size()==0) return false;
+                if(st.top()=='{') st.pop();
+                else return false;
+            }
+        }
+        if(st.size()==0) return true;
+        else return false;
+    }   
+    
+};
