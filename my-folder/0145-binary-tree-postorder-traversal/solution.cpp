1f30b377ece96bf50b615e3c48b9b03dcf65e67b
ravnoorsingh <160342867+ravnoorsingh@users.noreply.github.com>
2024-10-26 07:18:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.2 MB (62.59%)

--- my-folder/0145-binary-tree-postorder-traversal/solution.cpp
+++ my-folder/0145-binary-tree-postorder-traversal/solution.cpp
@@ -11,16 +11,32 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root,vector<int> &ans){
-        if(root==NULL) return;
-        helper(root->left,ans);
-        helper(root->right,ans);
-        ans.push_back(root->val);
+    // Recursive solution in Note
+
+    // Resursive solution
+    // void helper(TreeNode* root,vector<int> &ans){
+    //     if(root==NULL) return;
+    //     helper(root->left,ans);
+    //     helper(root->right,ans);
+    //     ans.push_back(root->val);
+    // }
+
+    void PostOrder(TreeNode* root,vector<int> &ans){
+        stack<TreeNode*> st;
+        if(root!=NULL) st.push(root);
+        while(st.size()>0){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if (temp->left!=NULL) st.push(temp->left);
+            if(temp->right!=NULL) st.push(temp->right);
+        }
+        reverse(ans.begin(),ans.end());
     }
 
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        helper(root,ans);
+        PostOrder(root,ans);
         return ans;
     }
 };
