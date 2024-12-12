9c4053bec4ecdb5dc2e26885ffd3e97ccfedcc08
RnoSin <160342867+ravnoorsingh@users.noreply.github.com>
2024-12-12 18:11:47 +0530

    Time: 19 ms (34.99%), Space: 38.7 MB (16.38%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,79 +1,19 @@
-
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int n = arr.size();
-        vector<int> ans(k);
-        if(x<arr[0]){
-            for(int i=0;i<k;i++){
-                ans[i]=arr[i];
-            }
-            return ans;
+        priority_queue<pair<int,int> > pq;
+        for(int num : arr){
+            int dist = abs(x-num);
+            pq.push({dist,num});
+            if(pq.size()>k) pq.pop();
         }
-        if(x>arr[n-1]){
-            int i = n-1;
-            int j = k-1;
-            while(j>=0){
-                ans[j]=arr[i];
-                j--;
-                i--;
-            }
-            return ans;
+        vector<int> res;
+        while(pq.size()>0){
+            auto x = pq.top();
+            res.push_back(x.second);
+            pq.pop();
         }
-        int low = 0;
-        int high = n-1;
-        bool flag = false;  // to check if x is presenr in the array or not
-        int mid = -1;
-        int t = 0;
-        // binary search
-        while(low<=high){
-            mid = low + (high-low)/2;
-            if(arr[mid]==x){
-                flag = true;
-                ans[t]= arr[mid];
-                t++;
-                break;
-            }
-            else if(arr[mid]>x) high =(mid - 1) ;
-            else low = mid + 1;
-        }
-        int lb = high; // lower bound
-        int ub = low;  //upper bound
-        if(flag == true){
-            lb = mid - 1;
-            ub = mid + 1;
-        }
-        while(t<k && lb>=0 && ub<=n-1){
-            int diff_1 = abs(x- arr[lb]);
-            int diff_2 = abs(x-arr[ub]);
-            if(diff_1<=diff_2){
-                ans[t]=arr[lb];
-                //t++;
-                lb--;
-            }
-            else { // diff_1 < diff_2
-                ans[t]=arr[ub];
-                //t++;
-                ub++;
-            }
-            t++;
-        }
-        if(lb<0){
-            while(t<k){
-                ans[t]=arr[ub];
-                ub++;
-                t++;
-            }
-        }
-        if(ub>n-1){
-            while(t<k){
-                ans[t]=arr[lb];
-                lb--;
-                t++;
-            }
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
-
+        sort(res.begin(),res.end());
+        return res;
     }
 };
